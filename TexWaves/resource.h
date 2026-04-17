57c4b1c6e5bc92e41a232a745e115b32dd61deb3
Sago <oliver030417@gmail.com>
2026-04-17 19:53:46 +0900

    뜨뜽
    가벼운 버튼 추가

--- TexWaves/resource.h
+++ TexWaves/resource.h
@@ -3,8 +3,12 @@
 // TexWaves.rc에서 사용되고 있습니다.
 //
 #define IDD_EDITOR_DLG                  101
-#define IDC_SLIDER1                     1001
 #define IDC_SLIDER_HEIGHT               1001
+#define IDC_BUTTON1                     1004
+#define IDC_BTN_LOAD_HEIGHMAP           1005
+#define IDC_CHECK1                      1006
+#define IDC_CHECK2                      1007
+#define IDC_CHECK_USE_HEIGHTMAP         1007
 
 // Next default values for new objects
 // 
@@ -12,7 +16,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        103
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1004
+#define _APS_NEXT_CONTROL_VALUE         1008
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
